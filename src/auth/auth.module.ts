edf57f720981586a9af8169bfb57459b448a5b6e
Alex Martin <alextravc.342@gmail.com>
2023-04-05 14:42:32 -0500

    [ADD]: jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Role } from './entities/role.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [UsersModule, TypeOrmModule.forFeature([Role])],
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    TypeOrmModule.forFeature([Role]),
+
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('jwtSecret'),
+        signOptions: { expiresIn: '3d' },
+      }),
+    }),
+
+    UsersModule,
+  ],
 })
 export class AuthModule {}
