208b1e7f303e1558413c6c17a037720161672154
Alex Martin <alextravc.342@gmail.com>
2023-04-05 14:05:35 -0500

    [ADD]: role entity

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Role } from './entities/role.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [UsersModule, TypeOrmModule.forFeature([Role])],
 })
 export class AuthModule {}
